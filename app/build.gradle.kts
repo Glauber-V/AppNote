61f485c5b614794085dad6fc96520614bb55e242
GlauberV <glaubervinicius2@hotmail.com>
2023-12-06 14:41:00 -0300

    build: migrate kapt to ksp
    remove kapt plugin
    add ksp plugin version 1.9.20-1.0.14
    
    build: update dependencies
    
    increase compileSdk and targetSdk from 33 to 34
    org.jetbrains.kotlin.android from 1.8.10 to 1.9.20
    com.google.dagger.hilt.android from 2.45 to 2.49
    com.google.dagger:hilt-android-compiler from 2.45 to 2.49
    com.google.dagger:hilt-android-testing from 2.45 to 2.49
    org.jetbrains.kotlin.android from 1.4.7 to 1.5.5
    androidx.core:core-ktx from 1.10.1 to 1.12.0
    androidx.compose:compose-bom from 2023.05.01 to 2023.10.01
    androidx.lifecycle:lifecycle-viewmodel-ktx 2.6.1 from to 2.6.2
    androidx.lifecycle:lifecycle-livedate-ktx 2.6.1 from to 2.6.2
    androidx.room:room-runtime from 2.5.1 to 2.6.1
    androidx.room:room-ktx from 2.5.1 to 2.6.1
    androidx.room:room-compiler from 2.5.1 to 2.6.1
    androidx.navigation:navigation-compose 2.5.3 from to 2.7.5
    androidx.navigation:navigation-testing from 2.5.3 to 2.7.5
    com.google.truth:truth from 1.1.2 to 1.1.5
    org.robolectric:robolectric from 4.10 to 4.11.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,18 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-android")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
-}
-
-kapt {
-    correctErrorTypes = true
 }
 
 android {
     namespace = "com.example.appnote"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.appnote"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -45,7 +40,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.5.5"
     }
 
     testOptions {
@@ -63,10 +58,10 @@ android {
 dependencies {
 
     // Core dependencies
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
 
     // Compose dependencies
-    val composeBom = platform("androidx.compose:compose-bom:2023.05.01")
+    val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
 
     implementation("androidx.compose.material3:material3")
@@ -79,17 +74,17 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata")
 
     // Architecture dependencies
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 
-    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
 
-    implementation("androidx.room:room-runtime:2.5.1")
-    implementation("androidx.room:room-ktx:2.5.1")
-    kapt("androidx.room:room-compiler:2.5.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 
-    implementation("com.google.dagger:hilt-android:2.45")
-    kapt("com.google.dagger:hilt-android-compiler:2.45")
+    implementation("com.google.dagger:hilt-android:2.49")
+    ksp("com.google.dagger:hilt-android-compiler:2.49")
 
     //  Unit test dependencies
     testImplementation("junit:junit:4.13.2")
@@ -100,16 +95,16 @@ dependencies {
     testImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
 
     testImplementation("androidx.arch.core:core-testing:2.2.0")
-    testImplementation("androidx.navigation:navigation-testing:2.5.3")
+    testImplementation("androidx.navigation:navigation-testing:2.7.5")
 
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 
-    testImplementation("com.google.truth:truth:1.1.2")
+    testImplementation("com.google.truth:truth:1.1.5")
 
-    testImplementation("com.google.dagger:hilt-android-testing:2.45")
-    kaptTest("com.google.dagger:hilt-android-compiler:2.45")
+    testImplementation("com.google.dagger:hilt-android-testing:2.49")
+    kspTest("com.google.dagger:hilt-android-compiler:2.49")
 
-    testImplementation("org.robolectric:robolectric:4.10")
+    testImplementation("org.robolectric:robolectric:4.11.1")
 
     testImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-    id("com.google.dagger.hilt.android") version "2.45" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
 }
